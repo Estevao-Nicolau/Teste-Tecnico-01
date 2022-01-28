98d6cdaa4e552e5b8e4ed27e3d5fbd8f4a27db28
Nicolau <estevao_nicolau@hotmail.com>
2022-01-28 11:29:46 -0300

    first commit

---
+++ main.dart
@@ -0,0 +1,17 @@
+int divisiveis(int num) {
+  if (num > 0) {
+    int total = 0;
+    for (int i = 0; i < num; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        total = total + i;
+      }
+    }
+    return total;
+  } else {
+    return -1;
+  }
+}
+
+void main() {
+  print(divisiveis(11));
+}
